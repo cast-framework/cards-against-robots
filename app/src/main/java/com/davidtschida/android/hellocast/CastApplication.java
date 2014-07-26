f630a0377b286ff1e33b22dac4771f89104e49b5
David Tschida <dmtschida1@gmail.com>
2014-07-26 13:30:56 -0400

    Initial app files.

---
+++ app/src/main/java/com/davidtschida/android/hellocast/CastApplication.java
@@ -0,0 +1,59 @@
+package com.davidtschida.android.hellocast;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.google.sample.castcompanionlibrary.cast.VideoCastManager;
+import com.google.sample.castcompanionlibrary.utils.Utils;
+
+/**
+ * The {@link Application} for this demo application.
+ */
+public class CastApplication extends Application {
+
+    public static final String APP_DESTRUCTION_KEY = "application_destruction";
+    public static final String FTU_SHOWN_KEY = "ftu_shown";
+    public static final String VOLUME_SELCTION_KEY = "volume_target";
+    public static final String TERMINATION_POLICY_KEY = "termination_policy";
+    public static final String STOP_ON_DISCONNECT = "1";
+    public static final String CONTINUE_ON_DISCONNECT = "0";
+
+
+    private static String APPLICATION_ID;
+    private static VideoCastManager mCastMgr = null;
+    public static final double VOLUME_INCREMENT = 0.05;
+    private static Context mAppContext;
+
+    /*
+     * (non-Javadoc)
+     * @see android.app.Application#onCreate()
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mAppContext = getApplicationContext();
+        APPLICATION_ID = "4860FC59";
+        Utils.saveFloatToPreference(getApplicationContext(),
+                VideoCastManager.PREFS_KEY_VOLUME_INCREMENT, (float) VOLUME_INCREMENT);
+
+    }
+
+    public static VideoCastManager getCastManager(Context context) {
+        if (null == mCastMgr) {
+            mCastMgr = VideoCastManager.initialize(context, APPLICATION_ID,
+                    null, null);
+            mCastMgr.enableFeatures(
+                    VideoCastManager.FEATURE_NOTIFICATION |
+                            VideoCastManager.FEATURE_LOCKSCREEN |
+                            VideoCastManager.FEATURE_DEBUGGING);
+
+        }
+        mCastMgr.setContext(context);
+        String destroyOnExitStr = Utils.getStringFromPreference(context,
+                TERMINATION_POLICY_KEY);
+        mCastMgr.setStopOnDisconnect(null != destroyOnExitStr
+                && STOP_ON_DISCONNECT.equals(destroyOnExitStr));
+        return mCastMgr;
+    }
+
+}
